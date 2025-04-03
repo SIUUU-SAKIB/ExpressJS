ec0ec0ffe82757fa8f5f1e1ce712c4bd2da78dc7
SIUUU-SAKIB <sakibmia7254@gmail.com>
2025-04-03 21:55:58 +0600

    second commit

--- index.js
+++ index.js
@@ -31,6 +31,61 @@ app.post(`/upload`, upload.single(`image`), async (req, res) => {
 app.get(`/`, (req, res) => {
   res.send(`server is working`);
 });
+app.get(`/movies`, (req, res) => {
+  const movies = [
+    {
+      name: "Inception",
+      release_year: 2010,
+      genre: "Sci-Fi",
+    },
+    {
+      name: "The Dark Knight",
+      release_year: 2008,
+      genre: "Action",
+    },
+    {
+      name: "Interstellar",
+      release_year: 2014,
+      genre: "Sci-Fi",
+    },
+    {
+      name: "Forrest Gump",
+      release_year: 1994,
+      genre: "Drama",
+    },
+    {
+      name: "The Matrix",
+      release_year: 1999,
+      genre: "Sci-Fi",
+    },
+    {
+      name: "Pulp Fiction",
+      release_year: 1994,
+      genre: "Crime",
+    },
+    {
+      name: "The Shawshank Redemption",
+      release_year: 1994,
+      genre: "Drama",
+    },
+    {
+      name: "The Godfather",
+      release_year: 1972,
+      genre: "Crime",
+    },
+    {
+      name: "Titanic",
+      release_year: 1997,
+      genre: "Romance",
+    },
+    {
+      name: "Gladiator",
+      release_year: 2000,
+      genre: "Action",
+    },
+  ];
+  res.send(movies)
+});
 app.listen(port, () =>
   console.log(`SERVER IN WORKING ON http://localhost:${port}`)
 );
